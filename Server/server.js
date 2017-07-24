264c6f339a388fb9dfde5254763d90c897168ddc
rcasto <rcasto@users.noreply.github.com>
2017-07-24 12:13:04 -0700

    add started website stuff

---
+++ Server/server.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var path = require('path');
+
+var port = process.env.PORT || 3000;
+var app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => res.sendFile('index.html'));
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
