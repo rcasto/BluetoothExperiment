048f9f733538b8d9b60a1ce0dda13f567393d5fc
rcasto <rcasto@users.noreply.github.com>
2017-07-25 18:00:00 -0700

    remove unneeded code

--- IoTClient/client.js
+++ IoTClient/client.js
@@ -1,6 +1,5 @@
 var WebSocket = require('ws');
 var events = require('events');
-var util = require('util');
 var config = require('./config.json');
 
 var wsProtocol = config.protocol === 'https' ? 'wss' : 'ws';
@@ -24,8 +23,7 @@ function ClientEmitter(info) {
         }
     });
 }
-util.inherits(ClientEmitter, events.EventEmitter);
-// ClientEmitter.prototype = Object.create(events.EventEmitter.prototype);
+ClientEmitter.prototype = Object.create(events.EventEmitter.prototype);
 
 ClientEmitter.prototype.connect = function () {
     var self = this;
@@ -44,37 +42,5 @@ ClientEmitter.prototype.connect = function () {
         console.error(error);
     });
 };
-
-// class ClientEmitter extends events.EventEmitter {
-//     constructor(info) {
-//         super();
-//         this.info = info;
-//         this.wsBuffer = [];
-//         this.ws = null;
-//         this.on('sensorData', (data) => {
-//             console.log(`Sensor data was called with: ${JSON.stringify(data)}`);
-//             data = Object.assign(data, this.info, {
-//                 timestamp: Date.now()
-//             });
-//             if (this.wsBuffer) {
-//                 this.wsBuffer.push(data);
-//             } else {
-//                 this.ws.send(JSON.stringify(data));
-//             }
-//         });
-//     }
-//     connect() {
-//         this.ws = new WebSocket(`${wsProtocol}://${config.domain}:${wsPort}`);
-//         this.ws.on('open', () => {
-//             console.log('Client connected to server');
-//             this.wsBuffer.forEach((msg) => this.ws.send(JSON.stringify(msg)));
-//             this.wsBuffer = null;
-//         });
-//         this.ws.on('message', (data) => {
-//             console.log(data);
-//         });
-//         this.ws.on('error', (error) => console.error(error));
-//     }
-// }
 
 module.exports = ClientEmitter;
